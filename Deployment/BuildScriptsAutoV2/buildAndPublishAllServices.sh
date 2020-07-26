d56f1d7b9ba1bf90cf2c1a5b78014f56142dcf1f
ssunderk <39213946+ssunderk@users.noreply.github.com>
2020-07-26 22:44:20 +0530

    Add files via upload

--- Deployment/BuildScriptsAutoV2/buildAndPublishAllServices.sh
+++ Deployment/BuildScriptsAutoV2/buildAndPublishAllServices.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Setting permissions to build scripts"
-chmod 777 /home/$1/engaze/git/Infra/Deployment/BuildScriptsAuto/build-publish-api-event.sh
-chmod 777 /home/$1/engaze/git/Infra/Deployment/BuildScriptsAuto/build-publish-api-managesubscriber.sh
+chmod 777 /home/$1/engaze/git/Infra/Deployment/BuildScriptsAutoV2/build-publish-api-event.sh
+chmod 777 /home/$1/engaze/git/Infra/Deployment/BuildScriptsAutoV2/build-publish-api-managesubscriber.sh
 
 echo ">>>>>>>>>>>>Build & Publish EventAPI"
-/home/$1/engaze/git/Infra/Deployment/BuildScriptsAuto/build-publish-api-event.sh $1 
+/home/$1/engaze/git/Infra/Deployment/BuildScriptsAutoV2/build-publish-api-event.sh $1 
 echo ">>>>>>>>>>>>Build & Publish Managesubscriber"
-/home/$1/engaze/git/Infra/Deployment/BuildScriptsAuto/build-publish-api-managesubscriber.sh $1
+/home/$1/engaze/git/Infra/Deployment/BuildScriptsAutoV2/build-publish-api-managesubscriber.sh $1
 
 echo "Running API EventAPI.Service.dll"
 cd /home/$1/engaze/api/event/
