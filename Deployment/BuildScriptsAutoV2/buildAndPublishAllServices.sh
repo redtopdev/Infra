4246c736c9bf892f5f1e8bdea4546fb697af133c
ssunderk <shyam_sunderkoottungal@mcafee.com>
2020-09-24 23:23:53 +0530

    nginx config fix

--- Deployment/BuildScriptsAutoV2/buildAndPublishAllServices.sh
+++ Deployment/BuildScriptsAutoV2/buildAndPublishAllServices.sh
@@ -14,15 +14,15 @@ echo ">>>>>>>>>>>>Build & Publish Location API"
 echo ">>>>>>>>>>>>Build & Publish PushNotification API"
 /home/$1/engaze/git/Infra/Deployment/BuildScriptsAutoV2/build-publish-api-pushnotification.sh $1
 
-echo "Running API EventAPI.Service.dll on 6000"
+echo "Running API EventAPI.Service.dll on port 6000"
 cd /home/$1/engaze/api/event/
-dotnet Engaze.Event.Service.dll --console  --urls http://localhost:6000 &
-echo "Running API SubScriber.Service.dll on 5000"
+dotnet Engaze.Event.Service.dll --console  --urls http://localhost:6000/ &
+echo "Running API SubScriber.Service.dll on port 5000"
 cd /home/$1/engaze/api/managesubscriber
-dotnet Subscriber.Service.dll --console  --urls http://localhost:5000 &
-echo "Running API ShareLocation.Service.dll  on 7000"
+dotnet Subscriber.Service.dll --console  --urls http://localhost:5000/ &
+echo "Running API ShareLocation.Service.dll on port 7000"
 cd /home/$1/engaze/api/location
-dotnet ShareLocation.Service.dll --console  --urls http://localhost:7000 &
-echo "Running API Notification.Service.dll on 8000"
+dotnet ShareLocation.Service.dll --console  --urls http://localhost:7000/ &
+echo "Running API Notification.Service.dll on port 8000"
 cd /home/$1/engaze/api/pushnotification
-dotnet Notification.Service.dll --console --urls http://localhost:8000 &
+dotnet Notification.Service.dll --console --urls http://localhost:8000/ &
